2cd21139a0814c7665015d57cf93d4651be72793
enio <enio.lopes@rvu.co.uk>
2021-06-21 11:25:07 +0100

    Change package for now

--- consumer_test.go
+++ consumer_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/aws/aws-sdk-go/service/kinesis/kinesisiface"
 
-	store "github.com/harlow/kinesis-consumer/store/memory"
+	store "github.com/eniolopes/kinesis-consumer/store/memory"
 )
 
 var records = []*kinesis.Record{
